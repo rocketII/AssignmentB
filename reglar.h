12a18c212a0c55fb4fb453bd42f2f62c0adbe0e7
rocketII <vangandr@gmail.com>
2016-02-19 08:44:33 +0100

    netbeans init

---
+++ reglar.h
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   reglar.h
+ * Author: bps
+ *
+ * Created on den 17 februari 2016, 13:17
+ */
+
+#ifndef REGLAR_H
+#define REGLAR_H
+//Är basklass
+//ska ha abstraktion och virtuealitet.
+class reglar
+{
+private:
+public:
+	
+};
+
+
+#endif /* REGLAR_H */
+
