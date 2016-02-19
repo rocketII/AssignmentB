687c95823acff29a66bd0f840ebcfbda4c1c6009
rlb <vangandr@gmail.com>
2016-02-19 08:26:09 +0100

    Added files via upload
    Added the files done before version management init.

---
+++ main.cpp
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: bps
+ *
+ * Created on den 15 februari 2016, 20:15
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv)
+{
+
+	return 0;
+}
+
---
+++ virke.h
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   virke.h
+ * Author: bps
+ *
+ * Created on den 17 februari 2016, 13:16
+ */
+
+#ifndef VIRKE_H
+#define VIRKE_H
+
+
+
+#endif /* VIRKE_H */
+
